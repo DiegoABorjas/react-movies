20215f71aa0da7c76203cb3bff0ab56625af3520
Diego <diegoborjas@gmail.com>
2023-11-16 18:19:48 -0600

    fixed link in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
         <>
         <footer id="footer" className="bg-dark mb-0 d-flex justify-content-center">
             <span className="bg-dark text-white flex gap-4 text-xl justify-center">
-                <a href="https://github.com/DiegoABorjas" target="_blank" rel="noreferrer noopener" >
+                <a href="https://diegoborjas.netlify.app/" target="_blank" rel="noreferrer noopener" >
                 <img style={{width: 80 + 'px'}} className="m-2" src={brandIcon} alt="github icon" />
                 </a>
                 <a href="https://github.com/DiegoABorjas" target="_blank" rel="noreferrer noopener" >
